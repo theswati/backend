9153d7098114bc94be0daac943b0dffcb127c401
Swati <jswati603@gmail.com>
2024-05-04 12:19:44 +0530

    apis

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.get('/api/products/slug/:slug',(req,res)=>{
 });
 
 
+app.get('/api/products/:id',(req,res)=>{
+    const product=data.products.find((x)=>x._id===req.params.id)
+      console.log(product)
+        if (product){
+            res.send(product)
+        }
+        else{
+            res.status(404).send({message:"Product not found"})
+        }
+})
+
+
     const port=process.env.PORT ||5001;
 
     app.listen(port,()=>{
