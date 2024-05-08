28756c706581e6893a17fbefc9c5ee02a2e5d3ab
Swati <jswati603@gmail.com>
2024-05-08 12:38:21 +0530

    models

--- data.js
+++ data.js
@@ -3,7 +3,7 @@
 const data={
     products:[
         {
-            _id:'1',
+            // _id:'1',
             name: 'Nike Slim shirt',
             slug: 'nike-slim-shirt',
             category: 'Shirts',
@@ -16,7 +16,7 @@ const data={
             description: 'high quality shirt',
           },
           {
-            _id:'2',
+            // _id:'2',
             name: 'Adidas Fit Shirt',
             slug: 'adidas-fit-shirt',
             category: 'Shirts',
@@ -29,7 +29,7 @@ const data={
             description: 'high quality product',
           },
           {
-            _id:'3',
+            // _id:'3',
             name: 'Nike Slim Pant',
             slug: 'nike-slim-pant',
             category: 'Pants',
@@ -42,7 +42,7 @@ const data={
             description: 'high quality product',
           },
           {
-            _id:'4',
+            // _id:'4',
             name: 'Adidas Fit Pant',
             slug: 'adidas-fit-pant',
             category: 'Pants',
--- server.js
+++ server.js
@@ -1,37 +1,59 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+
+dotenv.config();
+
+mongoose
+   .connect(process.env.MONGO_URI)
+   .then(()=>{
+    console.log('connected to db')
+})
+
+.catch((err)=>{
+    console.log(err.message)
+})
 
 const app=express();
 
-app.get("/api/products",(req,res)=>{
-    res.send(data.products)
-    });
+// app.get("/api/products",(req,res)=>{
+//     res.send(data.products)
+//     });
 
-app.get('/api/products/slug/:slug',(req,res)=>{
-        const product=data.products.find((x)=>x.slug===req.params.slug)
-        if (product){
-            res.send(product)
-        }
-        else{
-            res.status(404).send({message:"Product not found"})
-        }
-});
+// app.get('/api/products/slug/:slug',(req,res)=>{
+//         const product=data.products.find((x)=>x.slug===req.params.slug)
+//         if (product){
+//             res.send(product)
+//         }
+//         else{
+//             res.status(404).send({message:"Product not found"})
+//         }
+// });
 
 
-app.get('/api/products/:id',(req,res)=>{
-    const product=data.products.find((x)=>x._id===req.params.id)
-      console.log(product)
-        if (product){
-            res.send(product)
-        }
-        else{
-            res.status(404).send({message:"Product not found"})
-        }
-})
+// app.get('/api/products/:id',(req,res)=>{
+//     const product=data.products.find((x)=>x._id===req.params.id)
+//       console.log(product)
+//         if (product){
+//             res.send(product)
+//         }
+//         else{
+//             res.status(404).send({message:"Product not found"})
+//         }
+// })
 
+
+app.use('/api/seed',seedRouter);
+app.use('/api/products',productRouter);
 
     const port=process.env.PORT ||5001;
 
     app.listen(port,()=>{
         console.log(`server at http://localhost:${port}`)
     });
+
+
+    // ecom , 1CyNt0VO8BWlFcBm
