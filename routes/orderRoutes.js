e91cdd300e5efdef019195e148f3aac4dcde5373
Swati <jswati603@gmail.com>
2024-05-31 10:39:55 +0530

    payment

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -44,6 +44,31 @@ orderRouter.get(
         }
         
     })
-)
+);
+
+orderRouter.put(
+    '/:id/pay',
+    isAuth,
+    expressAsyncHandler(async(req,res)=>{
+        const order=await Order.findById(req.params.id);
+        if(order){
+            order.isPaid=true;
+            order.paidAt=Date.now();
+            order.PaymentResult={
+                id:req.body.id,
+                status:req.body.status,
+                update_time:req.body.update_time,
+                email_address:req.body.email_address,
+            };
+
+
+            const updateOrder=await order.save();
+            res.send({message:'Order Paid',order:updatedOrder});
+        }else{
+            res.status(404).send({message:'Order Not Found'});
+        }
+        
+    })
+);
 
 export default orderRouter;
